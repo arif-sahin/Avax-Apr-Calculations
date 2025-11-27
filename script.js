38d79897584d17f52efc9881cec3d8f8fdc7aef2
Abra-Eng <sahinarif051@gmail.com>
2025-11-27 19:52:01 +0300

    Initial Prototype without API

--- script.js
+++ script.js
@@ -1,85 +1,49 @@
-console.log('console');
+// Reward = (MaxSup - Supply) x (Stake/supply) x 
+// (StakingPeriod/mintingPeriod) x Effective Consumption Rate
+
 
 const Params = {
     MAXIMUM_SUPPLY: 720000000,
-    MINTING_PERIOD: 365 * 24 * 60 * 60, // 1 year in seconds
+    MinConsumptionRate: 0.10,
+    MaxConsumptionRate: 0.12,
+    VALIDATOR_FEE: 0.02,
 };
 
+function getStakeAmount() {
+    const stake = parseFloat(document.getElementById("myStake").value);
+    return stake;
+}
 
+function getDuration() {
+    const duration = parseFloat(document.getElementById("myDays").value);
+    return duration;
+}
 
+function getCurrentSupply() { // Will be done with API
+    return 465513067106358900/1000000000;
+}
 
+function calculate() {
+    //Remaining supply
+    const remainingSupply = Params.MAXIMUM_SUPPLY - getCurrentSupply();
 
+    // Share Calculation
+    const myShare = getStakeAmount() / getCurrentSupply();
+    
+    // Time 
+    const timeFactor = getDuration()/365;
 
+    //Rate Calculation, Longer duration = Higher rate
+    const percentofMaxTime = getDuration() / 365;
+    const myRate = Params.MinConsumptionRate + ((Params.MaxConsumptionRate - Params.MinConsumptionRate) * percentofMaxTime); 
 
+    // Gross Reward
+    const grossReward = remainingSupply * myShare * timeFactor * myRate;
 
-// const API_URL = "https://api.snowpeer.io/v1/monetary/current-total-supply?network=mainnet";
-// async function fetchFromSnowpeer() {
+    // Validator Fee Calculation and Net reward
+    const feeAmount = grossReward * Params.VALIDATOR_FEE;
+    
+    const netReward = grossReward - feeAmount;
 
-//             // The exact message we send to the P-Chain
-//             var payload = {
-//                 jsonrpc: "2.0",
-//                 id: 1,
-//                 method: "platform.getCurrentSupply",
-//                 params: {}
-//             };
-
-//             try {
-//                 // 1. Send the message
-//                 let response = await fetch(API_URL, {
-//                     method: "POST",
-//                     headers: { "Content-Type": "application/json" },
-//                     body: JSON.stringify(payload)
-//                 });
-
-//                 // 2. Get the answer
-//                 let data = await response.json();
-//                 console.log("API Answer:", data); // Check your console (F12) to see this!
-
-//                 if(data.result && data.result.supply) {
-//                     // 3. Convert nAVAX to AVAX (divide by 1 billion)
-//                     let supplyInAvax = data.result.supply / 1000000000;
-                    
-//                     // 4. Update the box
-//                     console.log(supplyInAvax);
-//                 } else {
-//                     console.log('nothing');
-//                 }
-
-//             } catch (err) {
-//                 console.error(err);
-
-//             }
-//         }
-
-
-
-// function getCurrentSupply() {
-//     const url = 'https://api.snowpeer.io/v1/monetary/current-total-supply?network=mainnet';
-
-//     const data = {
-//         currentSupply: "currentSupply",
-//     };
-
-//     const requestOptions = {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(data),
-//     };
-
-//     fetch(url, requestOptions)
-//     .then(response => {
-//         if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         outputElement.textContent = JSON.stringify(data, null, 2);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
-
-// }
+    document.getElementById("estimatedReward").textContent = netReward;
+}
