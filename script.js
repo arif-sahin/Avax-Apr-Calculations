d3922a20107d3ada5d0b05b4bf74c8efd64a63a5
Abra-Eng <sahinarif051@gmail.com>
2025-12-11 20:34:04 +0300

    Update UI elements, enhance input validation, and improve calculation logic for staking rewards

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
 // Reward = (MaxSup - Supply) x (Stake/supply) x 
 // (StakingPeriod/mintingPeriod) x Effective Consumption Rate
 
-
+const SECONDS_IN_DAY= 24 * 60 * 60;
 const Params = {
-    MINTING_PERIOD: 365 * 24 * 60 * 60,
+    MINTING_PERIOD: 365 * SECONDS_IN_DAY,
+    MIN_DURATION_SECONDS: 14 * SECONDS_IN_DAY, 
+    MAX_DURATION_SECONDS: 365 * SECONDS_IN_DAY,
     MAXIMUM_SUPPLY: 720000000,
     MinConsumptionRate: 0.10,
     MaxConsumptionRate: 0.12,
     VALIDATOR_FEE: 0.02,
-    MIN_STAKE: 25,
+    DELEGATOR_MIN_STAKE: 25,
+    VALIDATOR_MIN_STAKE: 2000,
     API_URL: "https://corsproxy.io/?" + encodeURIComponent("https://api.snowpeer.io/v1/monetary/current-total-supply?network=mainnet"),
 };
 let currentTotalSupply = NaN;
 let currentRole = 'delegator'; // default role
 
 window.onload = function() {
+    const now = new Date();
+    const minStakeDur = new Date(now.getTime() + (Params.MIN_DURATION_SECONDS * 1000)); // javascript use miliseconds
+
+    document.getElementById('startDate').value = localTime(now);
+    document.getElementById('endDate').value = localTime(minStakeDur);
+
     init();
     setRole('delegator');
 };
+
+function localTime(date) {
+    const offset = date.getTimezoneOffset() * 6000;
+    return (new Date(date - offset).toISOString().slice(0, 16));
+}
+
+// Sync 
+function syncFromNumber() {
+    const days = parseFloat(document.getElementById('myDays').value);
+    const start = new Date(document.getElementById('startDate').value);
+
+    if (!isNaN(start.getTime()) && !isNaN(days)) {
+        // Calculate new end date based on input days
+        const newEnd = new Date(start.getTime() + (days * SECONDS_IN_DAY * 1000));
+        document.getElementById('endDate').value = localTime(newEnd);
+
+        // Warning
+        const warningBox = document.getElementById('warningBox');
+        if (days < 14 || days > 365) {
+            warningBox.style.display = 'flex';
+        } else {
+            warningBox.style.display = 'none';
+        }
+    }
+}
+
+function syncFromDates() {
+    const start = new Date(document.getElementById('startDate').value);
+    const end = new Date(document.getElementById('endDate').value);
+
+    if (!isNaN(start.getTime()) && !isNaN(end.getTime())) {
+        const timeDiff = end - start;
+        const daysDiff = timeDiff / (SECONDS_IN_DAY * 1000);
+        const secondsDiff = timeDiff / 1000;
+
+        if (isNaN(daysDiff) || !isFinite(daysDiff)) return;
+
+        const warningBox = document.getElementById('warningBox');
+
+        if (secondsDiff < Params.MIN_DURATION_SECONDS || secondsDiff > Params.MAX_DURATION_SECONDS) {
+            warningBox.style.display = 'flex';
+        } else {
+            warningBox.style.display = 'none';
+        }
+
+        const numberInput = document.getElementById('myDays');
+
+        numberInput.value = Number.isInteger(daysDiff) ? daysDiff : daysDiff.toFixed(2);
+        document.getElementById('dayDisplay').innerText = daysDiff;
+    }
+}
 
 function getStakeAmount() {
     const stake = parseFloat(document.getElementById("myStake").value);
@@ -34,7 +94,13 @@ function getFee() {
     return fee;
 }
 
-async function fetchSupply() { 
+function getEffectiveConrate(durationInSeconds) {
+    const StakingPeriod = Math.min(durationInSeconds, Params.MINTING_PERIOD);
+    const ratio = StakingPeriod / Params.MINTING_PERIOD;
+    return (Params.MinConsumptionRate * (1 - ratio) + (Params.MaxConsumptionRate * ratio));
+}
+
+async function fetchSupply() { // Problem on Api
     const loading = document.getElementById('loading');
     
     try {
@@ -88,15 +154,16 @@ function setRole(role) {
     if(role === 'validator') {
         minText.innerText = "Minimum 2,000 AVAX required(Validator)";
         stake.value = 2000;
-        if(getStakeAmount() < 2000) {
-            alert('Minimum 2000 Avax');
+        if(getStakeAmount() < Params.VALIDATOR_MIN_STAKE) {
+            alert('Stake amount must be at least 2000 AVAX');
+            return;
         }
 
         feeSection.classList.add('disabled-section')
     } else {
         minText.innerText = "Minimium 25 AVAX required(Delegator)";
         stake.value = 25;
-        if(getStakeAmount() < Params.MIN_STAKE) {
+        if(getStakeAmount() < Params.DELEGATOR_MIN_STAKE) {
             alert("Stake amount must be at least 25 AVAX");
             return; // stop the function
         } 
@@ -106,37 +173,54 @@ function setRole(role) {
     //calculate();
 }
 
-function updateDayLabel() {
-    document.getElementById('dayDisplay').innerText = getDuration();
-}
-
 function calculate() {
     //Remaining supply
     const remainingSupply = Params.MAXIMUM_SUPPLY - currentTotalSupply;
-    console.log(remainingSupply);
 
     // Share Calculation
     const myShare = getStakeAmount() / currentTotalSupply;
-    console.log(myShare);
     
-    // Time 
-    const timeFactor = getDuration()/365;
+    // Duration in Seconds 
+    const start = new Date(document.getElementById('startDate').value);
+    const end = new Date(document.getElementById('endDate').value);
+    let durationSeconds = (end - start) / 1000;
+
+    if (durationSeconds < Params.MIN_DURATION_SECONDS) {
+        document.getElementById('estimatedReward').innerText = "---";
+        document.getElementById('apyOutput').innerText = "---";
+        document.getElementById('aprOutput').innerText = "---";
+        return;
+    }
 
     //Rate Calculation, Longer duration = Higher rate
-    const percentofMaxTime = getDuration() / 365;
-    const effectiveRate = Params.MinConsumptionRate + ((Params.MaxConsumptionRate - Params.MinConsumptionRate) * percentofMaxTime); 
-
+    const effectiveRate = getEffectiveConrate(durationSeconds);
+    
+    // time factor
+    const timeFactor = durationSeconds / Params.MINTING_PERIOD;
+    
     // Gross Reward
     let reward = remainingSupply * myShare * timeFactor * effectiveRate;
 
-    // Fee and Net reward
+    // For Delegator, Fee and Net reward
     if (currentRole === 'delegator') {
         const feeCut = reward * (getFee() / 100);
         reward = reward - feeCut;
     }
 
-    const apy = (reward / getStakeAmount()) * (365 / getDuration()) * 100;
-    
+    const simpleReturn = reward / getStakeAmount();
+    const apr = simpleReturn * (Params.MINTING_PERIOD / durationSeconds) * 100;
+    const periodsPerYear = Params.MINTING_PERIOD / durationSeconds;
+    const apy = (Math.pow(1 + simpleReturn, periodsPerYear) - 1) * 100;
+
     document.getElementById('estimatedReward').innerText = reward.toFixed(6);
+    document.getElementById('aprOutput').textContent = apr.toFixed(2);
     document.getElementById('apyOutput').textContent = apy.toFixed(2);
+
+    // Projections
+    const projectedYearly = getStakeAmount() * (apy / 100);
+
+    document.getElementById('projDaily').innerText = (projectedYearly / 365).toFixed(4);
+    document.getElementById('projWeekly').innerText = (projectedYearly / 52).toFixed(4);
+    document.getElementById('projMonthly').innerText = (projectedYearly / 12).toFixed(4);
+    document.getElementById('projYearly').innerText = projectedYearly.toFixed(4);
 }
